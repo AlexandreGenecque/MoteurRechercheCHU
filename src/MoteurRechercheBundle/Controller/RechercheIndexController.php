909475329d52ef92fbf00bca940aa9a52224470b
Alexandre Genecque <alexandre.genecque@gmail.com>
2016-05-02 09:37:19 +0200

    Ajout Index

---
+++ src/MoteurRechercheBundle/Controller/RechercheIndexController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace MoteurRechercheBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use MoteurRechercheBundle\Entity\Analyse;
+use MoteurRechercheBundle\Entity\ConditionPrelevement;
+use MoteurRechercheBundle\Entity\ConservationAvantTransport;
+use MoteurRechercheBundle\Entity\DelaiResultat;
+use MoteurRechercheBundle\Entity\FichePrescription;
+use MoteurRechercheBundle\Entity\Laboratoire;
+use MoteurRechercheBundle\Entity\MicroOrganisme;
+use MoteurRechercheBundle\Entity\NatureMatrice;
+use MoteurRechercheBundle\Entity\NaturePrelevement;
+use MoteurRechercheBundle\Entity\PrincipeMethode;
+use MoteurRechercheBundle\Entity\RenseignementClinique;
+use MoteurRechercheBundle\Entity\Secteur;
+use MoteurRechercheBundle\Entity\Transport;
+
+
+
+/**
+ * Analyse controller.
+ *
+ */
+class RechercheIndexController extends Controller
+{
+
+    public function indexAction()
+    {
+        return $this->render('rechercheIndex/index.html.twig');
+    }
+}
