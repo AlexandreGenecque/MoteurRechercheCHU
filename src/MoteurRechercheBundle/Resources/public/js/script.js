12b8af76e2f1ecc264e9bac733144e2c66d121dc
Alexandre Genecque <alexandre.genecque@gmail.com>
2016-05-05 10:22:43 +0200

    Modif de style bis (genecque)
    correction code CSS

--- src/MoteurRechercheBundle/Resources/public/js/script.js
+++ src/MoteurRechercheBundle/Resources/public/js/script.js
@@ -1,5 +1,7 @@
 function RAZvalue()
 {
+
+	
 	document.getElementById("btnRecherche").value = "";
 	document.getElementById("selectLabo").value = "Defaut";
 	document.getElementById("selectMicroOrg").value = "a";
