b4300513b560d2fa3a0ccd605eed5cb66dec52a1
Alexandre Genecque <alexandre.genecque@gmail.com>
2016-06-25 20:09:06 +0200

    PDF avec 1 analyse

--- src/MoteurRechercheBundle/Controller/DefaultController.php
+++ src/MoteurRechercheBundle/Controller/DefaultController.php
@@ -53,21 +53,23 @@ class DefaultController extends Controller
 	//Output envoit le document PDF au navigateur internet
 	return new Response($html2pdf->Output('nom-du-pdf.pdf'), 200, array('Content-Type' => 'application/pdf'));
 		       return $this->render('rechercheIndex/index.html.twig');
-//
-<<<<<<< Updated upstream
+
 	*/	 
 
 	//return $this->render('rechercheIndex/analyseSimplePDF.html.twig', array('analyses' => $analyses));
 
 	//$analyses = $em->getRepository('MoteurRechercheBundle:Analyse')->findall();
 
- 	$html = $this->renderView('rechercheIndex/analyseSimplePDF.html.twig', array(
+	
+
+    $html = $this->renderView('rechercheIndex/analyseSimplePDF.html.twig', array(
     'analyses'  => $analyses
-));
+	));
 
 	return new Response($this->get('knp_snappy.pdf')->getOutputFromHtml($html, array(
 	 'default-header'=>false)), 200,
 	 array('Content-Type' => 'application/pdf', 'Content-Disposition' => 'attachment; filename="Test.pdf"'));
+<<<<<<< Updated upstream
 
 	 	//return $this->render('rechercheIndex/analyseSimplePDF.html.twig', array('analyses' => $analyses));
 
@@ -80,10 +82,13 @@ class DefaultController extends Controller
 return new Response($this->get('knp_snappy.pdf')->getOutputFromHtml($html, array('orientation'=>'Landscape',
  'default-header'=>true)), 200,
  array('Content-Type' => 'application/pdf', 'Content-Disposition' => 'attachment; filename="Test.pdf"'));
+=======
+	
+	
+	
+>>>>>>> Stashed changes
 
-/*$pdf = $this->get('knp_snappy.pdf')->getOutputFromHtml($html,
-                                   array('orientation'=>'Landscape',
-                                         'default-header'=>true));*/
+	//return $this->render('rechercheIndex/analyseSimplePDF.html.twig', array('analyses' => $analyses));
 
 
     }
